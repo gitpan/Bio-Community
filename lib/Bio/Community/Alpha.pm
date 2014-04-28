f5301be07b1496b4f60386fc0be8fc04d332170a
Florent Angly <fangly@cpan.org>
2014-04-28 14:39:29 +0000

    Import of FANGLY/Bio-Community-0.001006 from CPAN.
    gitpan-cpan-distribution: Bio-Community
    gitpan-cpan-version:      0.001006
    gitpan-cpan-path:         FANGLY/Bio-Community-0.001006.tar.gz
    gitpan-cpan-author:       FANGLY
    gitpan-cpan-maturity:     released

--- lib/Bio/Community/Alpha.pm
+++ lib/Bio/Community/Alpha.pm
@@ -102,17 +102,18 @@ has community => (
  Usage   : my $type = $alpha->type;
  Args    : String for the desired type of alpha diversity ('observed' by default).
 
-           Richness (or estimated number of species):
+           Richness (estimated number of species), based on counts, not relative
+           abundance:
             * observed :  C<S>
             * menhinick:  C<S/sqrt(n)>, where C<n> is the total counts (observations).
             * margalef : C<(S-1)/ln(n)>
             * chao1    : Bias-corrected chao1 richness, C<S+n1*(n1-1)/(2*(n2+1))>
                          where C<n1> and C<n2> are the number of singletons and
                          doubletons, respectively. Particularly useful for data
-                         skewed toward the low-abundance species, e.g. microbial.
-                         Based on counts, not relative abundance.
-            * ace      : Abundance-based Coverage Estimator (ACE). Based on
-                         counts, not relative abundance.
+                         skewed by low-abundance species, e.g. microbial data.
+            * ace      : Abundance-based Coverage Estimator (ACE).
+            * jack1    : First-order jackknife richness estimator, C<S+n1>.
+            * jack2    : Second-order jackknife richness estimator, C<S+2*n1-n2>.
 
            Evenness (or equitability):
             * buzas      : Buzas & Gibson's (or Sheldon's) evenness, C<e^H/S>.
@@ -216,20 +217,28 @@ method _chao1 () {
    # Calculate Chao's bias-corrected chao1 richness
    # We use the bias-corrected version because it is always defined, even if
    # there are no doubletons, contrary to the non-bias corrected version
-   my $d = 0;
-   my ($n1, $n2) = (0, 0); # singletons and doubletons
+   # http://www.uvm.edu/~ngotelli/manuscriptpdfs/Chapter%204.pdf page 40
    my $community = $self->community;
-   while (my $member = $community->next_member('_alpha_chao1')) {
+   my ($n1, $n2) = $self->__calc_xtons($community);
+   return $community->get_richness + ($n1*($n1-1)) / (2*($n2+1));
+}
+
+
+method __calc_xtons ($community) {
+   # Return the number of singleton and doubletons in the given community
+   my ($n1, $n2) = (0, 0);
+   while (my $member = $community->next_member('_alpha_calc_xtons')) {
       my $c = $community->get_count($member);
       if ($c == 1) {
          $n1++;
       } elsif ($c == 2) {
          $n2++;
       } elsif ( $c != int($c) ) {
-         $self->throw("Got count $c but can only compute chao1 on integer numbers");
+         $self->throw("Got count $c but can only compute metric '".$self->type.
+            "' on integer abundance data");
       }
    }
-   return $community->get_richness + ($n1*($n1-1)) / (2*($n2+1));
+   return $n1, $n2;
 }
 
 
@@ -240,8 +249,9 @@ method _ace () {
    my $community = $self->community;
    if ($community->get_richness > 0) {
       my $thresh = 10;
-      my ($s_rare, $s_abund) = (0, 0); # number of rare, and abundant (>10) species
       my @F = (0) x $thresh; # number of singletons, doubletons, tripletons, ... 10-tons
+      my ($s_rare, $s_abund) = (0, 0); # number of rare, and abundant (>10) species
+      my $n_rare = 0;
       while (my $member = $community->next_member('_alpha_ace')) {
          my $c = $community->get_count($member);
          if ($c > $thresh) {
@@ -253,19 +263,18 @@ method _ace () {
             } else {
                $F[$c-1]++;
             }
+            $n_rare += $c;
          }
       }
-      my ($n_rare, $tmp_sum) = (0, 0);
-      for my $i (0.. $thresh) {
-         my $add = $i * $F[$i-1];
-         $n_rare  += $add;
-         $add *= $i-1;
-         $tmp_sum += $add;
-      }
       if ($n_rare == $F[0]) {
-         # ACE is not defined when all rare species are singletons
-         $d = undef;
+         # ACE is not defined when all rare species are singletons.
+         # Fall back to chao1 (advised by Anne Chao, implemented in EstimateS)
+         $d = $self->_chao1();
       } else {
+         my $tmp_sum = 0;
+         for my $k (2 .. $thresh) {
+            $tmp_sum += $k * ($k-1) * $F[$k-1];
+         }
          my $C = 1 - $F[0]/$n_rare;
          my $gamma = ($s_rare * $tmp_sum) / ($C * $n_rare * ($n_rare-1)) - 1;
          $gamma = max($gamma, 0);
@@ -273,6 +282,24 @@ method _ace () {
       }
    }
    return $d;
+}
+
+
+method _jack1 () {
+   # Calculate first-order jackknife richness estimator.
+   # http://www.uvm.edu/~ngotelli/manuscriptpdfs/Chapter%204.pdf page 41
+   my $community = $self->community;
+   my ($n1, $n2) = $self->__calc_xtons($community);
+   return $community->get_richness + $n1;
+}
+
+
+method _jack2 () {
+   # Calculate second-order jackknife richness estimator.
+   # http://www.uvm.edu/~ngotelli/manuscriptpdfs/Chapter%204.pdf page 41
+   my $community = $self->community;
+   my ($n1, $n2) = $self->__calc_xtons($community);
+   return $community->get_richness + 2 * $n1 - $n2;
 }
 
 
